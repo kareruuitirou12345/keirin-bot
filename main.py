0532f69cd73212933c3af9df789dfeac8d1be9f7
kareruuitirou12345 <kareruuitirou123@gmail.com>
2026-03-16 22:45:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 
+session = requests.Session()
+
+session.headers.update({
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://keirin-station.com/"
+})
+
 url = "https://keirin-station.com/keirindb/race/member/31/20260316/5/"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
-    "Accept-Language": "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Referer": "https://keirin-station.com/",
-}
-
-res = requests.get(url, headers=headers)
+res = session.get(url)
 
 print(res.status_code)
-print(res.text[:1000])
