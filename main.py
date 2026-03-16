fd3e3cb9a370735c63db1ed04a89b8ba47d65e6b
kareruuitirou12345 <kareruuitirou123@gmail.com>
2026-03-16 22:57:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import os
-
-WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
 url = "https://keirin.netkeiba.com/race/entry/?race_id=202603163108"
 
@@ -14,19 +11,13 @@ res = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(res.text, "html.parser")
 
-rows = soup.select(".RaceTable01 tbody tr")
+rows = soup.select("table.RaceTable01 tbody tr")
 
-msg = "🚴 出走表\n"
+print("row count:", len(rows))
 
 for row in rows:
 
-    number = row.select_one(".Umaban").text.strip()
-    name = row.select_one(".Name").text.strip()
-    score = row.select_one(".Score").text.strip()
+    name = row.select_one(".Name")
 
-    msg += f"\n{number} {name} ({score})"
-
-requests.post(
-    WEBHOOK,
-    json={"content": msg}
-)
+    if name:
+        print(name.text.strip())
