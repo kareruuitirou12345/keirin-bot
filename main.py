0d665f75b50979899b2e9c0b1a9e05f5b6216670
kareruuitirou12345 <kareruuitirou123@gmail.com>
2026-03-16 22:54:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests
+from bs4 import BeautifulSoup
+import os
 
-session = requests.Session()
+WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
-session.headers.update({
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://keirin-station.com/"
-})
+url = "https://keirin.netkeiba.com/race/entry/?race_id=202603163108"
 
-url = "https://keirin-station.com/keirindb/race/member/31/20260316/5/"
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
-res = session.get(url)
+res = requests.get(url, headers=headers)
 
-print(res.status_code)
+soup = BeautifulSoup(res.text, "html.parser")
+
+rows = soup.select(".RaceTable01 tbody tr")
+
+msg = "🚴 出走表\n"
+
+for row in rows:
+
+    number = row.select_one(".Umaban").text.strip()
+    name = row.select_one(".Name").text.strip()
+    score = row.select_one(".Score").text.strip()
+
+    msg += f"\n{number} {name} ({score})"
+
+requests.post(
+    WEBHOOK,
+    json={"content": msg}
+)
