55b686e99eddef560210b93b1344d92a9d64e815
kareruuitirou12345 <kareruuitirou123@gmail.com>
2026-03-16 22:42:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://keirin-station.com/keirindb/race/member/31/20260316/5/"
-res = requests.get(url)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36"
+}
+
+res = requests.get(url, headers=headers)
 
 print(res.status_code)
-print(res.text[:2000])
+print(res.text[:1000])
